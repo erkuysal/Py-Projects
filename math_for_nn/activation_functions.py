0c933e01f9269d86c7c09ae1d2993127659b1208
erkuysal <synthaine@outlook/com>
2023-07-28 22:24:44 +0300

    Function names added.

--- math_for_nn/activation_functions.py
+++ math_for_nn/activation_functions.py
@@ -1,9 +1,35 @@
-# This file is created for tracking progress.
+# This file is created to better understanding the maths for ML.
 
 # Threshold function // Binary Step function
-
 def threshold(x):
     if x <= 0:
         return False
     else:
         return True
+
+def Sigmoid():
+
+# Ridge Activation Functions
+
+def  Linear():
+
+def ReLu():
+
+def Heaviside():
+
+def Logistic():
+
+
+# Radial Activation Functions
+
+def Gaussian():
+
+def Multiquadratics():
+
+def Inv_Multiquadratics():
+
+# Folding Activation Functions
+
+
+
+
