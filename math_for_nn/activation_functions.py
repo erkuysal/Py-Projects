ae7416bbce4ca4acb06bb3964c32a009ae830ca8
erkuysal <synthaine@outlook/com>
2023-08-04 04:06:04 +0300

    Activation functions added

--- math_for_nn/activation_functions.py
+++ math_for_nn/activation_functions.py
@@ -7,26 +7,94 @@ def threshold(x):
     else:
         return True
 
-def Sigmoid():
+def sigmoid(x):
+    """
+        Calculate the sigmoid function for a given input.
+
+        Parameters:
+            x (float): The input value.
+
+        Returns:
+            float: The result of the sigmoid function.
+        """
+    # Calculate e^(-x) using the Taylor series expansion
+    ex_neg_x = 1.0
+    term = 1.0
+    for i in range(1, 100):  # 100 terms are sufficient for a good approximation
+        term *= (-x) / i
+        ex_neg_x += term
+
+    # Calculate and return the sigmoid function
+    return 1 / (1 + ex_neg_x)
 
 # Ridge Activation Functions
 
-def  Linear():
+def  linear(m, x, n):
+    """
+       Calculate the value of a linear function for a given input.
 
-def ReLu():
+       Parameters:
+           x (float): The input value.
+           m (float): The slope of the line.
+           n (float): The y-intercept of the line.
 
-def Heaviside():
+       Returns:
+           float: The result of the linear function.
+       """
+    return m * x + n
 
-def Logistic():
+def relu(x):
+    """
+       Rectified Linear Unit (ReLU) function.
+
+       Parameters:
+           x (float): The input value.
+
+       Returns:
+           float: The result of the ReLU function.
+       """
+    return max(0, x)
+
+def heaviside(x):
+    """
+        Heaviside step function.
+
+        Parameters:
+            x (float): The input value.
+
+        Returns:
+            float: The result of the Heaviside function.
+        """
+    return 1 if x >= 0 else 0
+
+def logistic(x):
+    """
+       Logistic function (sigmoid function).
+
+       Parameters:
+           x (float): The input value.
+
+       Returns:
+           float: The result of the logistic function.
+       """
+    # Calculate e^(-x) using the Taylor series expansion
+    ex_neg_x = 1.0
+    term = 1.0
+    for i in range(1, 100):  # 100 terms are sufficient for a good approximation
+        term *= (-x) / i
+        ex_neg_x += term
+
+    # Calculate and return the logistic function
+    return 1 / (1 + ex_neg_x)
 
 
 # Radial Activation Functions
 
-def Gaussian():
+def gaussian():
 
-def Multiquadratics():
+def multiquadratics():
 
-def Inv_Multiquadratics():
+def inv_Multiquadratics():
 
 # Folding Activation Functions
 
